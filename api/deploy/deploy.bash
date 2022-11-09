6a73ce9aa4cb037afd70ba87529b0bdc9a643ea4
kompotkot <sumarokovsergei@yandex.ru>
2022-11-09 11:44:16 +0000

    Rewrite envs during deployment

--- api/deploy/deploy.bash
+++ api/deploy/deploy.bash
@@ -47,7 +47,7 @@ HOME=/root /usr/local/go/bin/go install github.com/bugout-dev/checkenv@latest
 echo
 echo
 echo -e "${PREFIX_INFO} Retrieving addition deployment parameters"
-AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" /root/go/bin/checkenv show aws_ssm+entity:true >> "${PARAMETERS_ENV_PATH}"
+AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" /root/go/bin/checkenv show aws_ssm+entity:true > "${PARAMETERS_ENV_PATH}"
 
 echo
 echo

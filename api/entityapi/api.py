1c6e4a704b7fdffc7fa8f099941056bd5bfc1f24
kompotkot <sumarokovsergei@yandex.ru>
2022-12-13 14:36:00 +0000

    Removed print

--- api/entityapi/api.py
+++ api/entityapi/api.py
@@ -193,7 +193,6 @@ async def add_entity_handler(
         title, tags, content = actions.parse_entity_to_entry(
             create_entity=create_request
         )
-        print(title, tags, content)
 
         response: BugoutJournalEntry = bc.create_entry(
             token=token,
